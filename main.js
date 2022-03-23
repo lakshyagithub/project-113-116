fa25257fb59b3214c94c24252f52c1ce1bfcf360
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-03-23 19:01:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
+noseX=0;
+noseY=0;
+
 function preload() {
-    
-    }
-    function setup() {
-    canvas = createCanvas(300,300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300, 300);
-    video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-    }
-    function gotPoses(results){
-    if(results.length > 0) {
-    console.log(results);
-    console.log("nose x = "+results[0].pose.nose.x);
-    console.log("nose y = "+results[0].pose.nose.y);
-    }
-    }
-    function modelLoaded() {
-    console.log("PoseNet is Intialized");
-    }
-    function draw() {
-    image(video, 0, 0, 300, 300);
-    }
-    function take_snapshot() {
-    save("MyFliter.png");
-    }
+m = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+function setup() {
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function gotPoses(results){
+if(results.length > 0) {
+console.log(results);
+noseX=results[0].pose.nose.x-12;
+noseY=results[0].pose.nose.y-12;
+console.log("nose x = "+noseX);
+console.log("nose y = "+noseY);
+}
+}
+function modelLoaded() {
+console.log("PoseNet is Intialized");
+}
+function draw() {
+image(video, 0, 0, 300, 300);
+image(m, noseX, noseY, 30, 30);
+}
+function take_snapshot() {
+save("MyFilter.png");
+}

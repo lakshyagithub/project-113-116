c1d08496b3ff702e28ac848ec672858078d71ba8
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-03-24 09:42:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ poseNet.on('pose', gotPoses);
 function gotPoses(results){
 if(results.length > 0) {
 console.log(results);
-noseX=results[0].pose.nose.x-12;
-noseY=results[0].pose.nose.y-12;
+noseX=results[0].pose.nose.x-13;
+noseY=results[0].pose.nose.y;
 console.log("nose x = "+noseX);
 console.log("nose y = "+noseY);
 }
